4bfca2b1f4c5fb9745e8bec42c4e377bd1996932
Fabiano Bordinoski <root@rootx39.com>
2017-10-29 02:47:23 -0200

    FB 06 - Iniciando estrutura index.php

---
+++ wp-content/themes/verissimofotografia/index.php
@@ -0,0 +1,26 @@
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<title>Verissimo Fotografia - Newborn - Casal - Casamento - Infantil - Feminina - Família - Eventos</title>
+</head>
+<body <?php body_class(); ?>>
+<header>
+	<div class="barra-topo">
+		<div class="redes-sociais"></div>
+		<div class="logo"></div>
+	</div>
+</header>
+<div class="conteudo">
+	<main>
+		<div class="fotos">
+			<div class="foto-1"></div>
+			<div class="foto-2"></div>
+			<div class="foto-3"></div>
+		</div>
+		<div class="barra-servicos"></div>
+	</main>
+</div>
+
+	
+</body>
+</html>
