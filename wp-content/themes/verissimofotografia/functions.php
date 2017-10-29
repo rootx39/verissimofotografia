d660ec6213e7b8053bf8742a94164cb456ae56e1
Fabiano Bordinoski <root@rootx39.com>
2017-10-29 10:12:10 -0200

    FB 20171029 - 10:11 | Correção Arquivo functions.php, Alteração header.php, alteração template.css

--- wp-content/themes/verissimofotografia/functions.php
+++ wp-content/themes/verissimofotografia/functions.php
@@ -1,9 +1,19 @@
 <?php 
-
+// Função para scripts e estilos
 function load_scripts(){
 	wp_enqueue_style('template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all');
 	wp_enqueue_script('template', get_template_directory_uri() . '/js/template.js', array(), null, true);
-	wp_enqueue_script('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css', array(), '4.0.0', true);
-	wp_enqueue_script('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css', array('jquery'), '4.0.0', true);
+
+	wp_enqueue_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css', array(), null, true);
+	wp_enqueue_script('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js', array('jquery'), null, true);
+
+	wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:300,400', false);
 }
 add_action('wp_enqueue_scripts', 'load_scripts');
+
+//Função para menus
+register_nav_menus(
+	array(
+		'meu_menu_principal' => 'Menu Principal'
+	)
+);
--- wp-content/themes/verissimofotografia/header.php
+++ wp-content/themes/verissimofotografia/header.php
@@ -1,12 +1,16 @@
 <html lang="pt-br">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Verissimo Fotografia - Newborn - Casal - Casamento - Infantil - Feminina - Família - Eventos</title>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <header>
-	<div class="barra-topo">
-		<div class="logo">Logo</div>
+	<div class="barra-topo container">
+		<div class="row">
+			<div class="logo col-md-3">Verissimo Fotografia</div>
+			<div class="menu-principal col-md-9"><?php wp_nav_menu( array('theme_location' => 'meu_menu_principal')) ?></div>			
+		</div>
 	</div>
 </header>
