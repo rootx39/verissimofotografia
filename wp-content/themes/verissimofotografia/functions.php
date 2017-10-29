5943c403edd1d468c3abcfe7ebc7489fcf7832a6
Fabiano Bordinoski <root@rootx39.com>
2017-10-29 10:50:00 -0200

    FB 20171029 - 10:49 | Correção Arquivo functions.php, alteração template.css

--- wp-content/themes/verissimofotografia/functions.php
+++ wp-content/themes/verissimofotografia/functions.php
@@ -4,7 +4,7 @@ function load_scripts(){
 	wp_enqueue_style('template', get_template_directory_uri() . '/css/template.css', array(), '1.0', 'all');
 	wp_enqueue_script('template', get_template_directory_uri() . '/js/template.js', array(), null, true);
 
-	wp_enqueue_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css', array(), null, true);
+	wp_enqueue_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css', array(),'4.0.0', 'all');
 	wp_enqueue_script('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js', array('jquery'), null, true);
 
 	wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:300,400', false);
