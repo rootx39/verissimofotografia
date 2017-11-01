313e1f90fc3e5349e178b6124b68cb23ba2c7eda
Fabiano Bordinoski <root@rootx39.com>
2017-11-01 01:52:11 -0200

    20171101 - 01:51 | Alterado tamanho de img da galeria portfolio para 1200x800 na média.

--- wp-content/themes/verissimofotografia/index.php
+++ wp-content/themes/verissimofotografia/index.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
 	<div class="container corbranca">
-			<img class="img-responsive col-md-12" src="<?php header_image(); ?>" heigth="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Cabeçalho">
+			<img class="img-responsive img-padding col-md-12" src="<?php header_image(); ?>" heigth="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Cabeçalho">
 			<main >
 				<section class="text-center"><p>Newborn - Casal - Casamento - Infantil - Feminina - Família - Eventos</p></section>
-				<section>
-					<div class="col-md-12">
+				<div class="row mid-bot">
+					<div class="col-md-7">
 						<form action="script/enviar.php" method="post">
-							<h2 class="text-left">Contate-nos</h2>
+							<h2 class="text-left txt-branco">Contate-nos</h2>
 							<div class="form-group has-success has-feedback">
 								<input class="form-control" name="name" type="text" placeholder="Nome" />
 							</div>
@@ -21,7 +21,11 @@
 							</div>
 						</form>
 					</div>
-				</section>
+					<div class="col-md-5">
+						<h2 class="text-left txt-branco">Localização</h2>
+					    <iframe width="100%" height="400" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDIuooHn3SefMXcI-t7f0_9LUNxVF1WOdQ&amp;q=Travessa+Para%C3%ADba&amp;zoom=163"></iframe>
+					</div>
+				</div>
 			</main>
 	</div>
 <?php get_footer(); ?>
