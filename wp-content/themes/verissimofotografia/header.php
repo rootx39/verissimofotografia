c21e2539de080c677ae7efe40923f88cc1b970b0
Fabiano Bordinoski <root@rootx39.com>
2017-11-07 16:18:02 -0200

    20171107 - 16:16 | Correção V1.2 - Organizando pagina portfolio.php

--- wp-content/themes/verissimofotografia/header.php
+++ wp-content/themes/verissimofotografia/header.php
@@ -9,7 +9,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title><?php bloginfo('title'); ?></title>
+	<title><?php bloginfo('name'); wp_title(); ?></title>
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
